c650916c8fe74253bb7c397e0a2d98bb2b178d53
Bikash Nath <splittergame91@gmail.com>
2022-11-29 20:33:57 +0530

    variables types, uses

---
+++ variable.js
@@ -0,0 +1,39 @@
+//three types of variable are there in js: let, const, var
+
+
+
+let x;    //variable with undefined value
+
+
+x="name"
+console.log(x)
+
+
+
+
+// let variable is changeable
+let a = 7  //let variable with int datatype
+
+
+// var is for old browser
+var b = "String"  //var variable with string datatype
+
+
+//const variable is fixed
+const bool = true   //const variable with boolean datatype
+
+
+// undeclared variable
+c = 78
+
+console.log(a)
+console.log(b)
+console.log(bool)
+console.log(c)
+
+
+let d = 5+ 8 + "5"  //quotation int will be treated as string & concatenate
+
+console.log(d)  //
+
+
