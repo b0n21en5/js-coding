0cb1bad8322cc2d04ee5da13fad4964a8e7b2b4a
Bikash Nath <splittergame91@gmail.com>
2022-11-29 23:46:50 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,61 @@
+// assignment operator used to assign value to variables
+let d = "string"
+d += 1  //increment assignment operator
+d -= 1  //decrement assignment 
+d *= 2  //multiplication assignment
+d /= 3  // division assignment
+d **= 2  //exponential assignment
+
+
+
+
+// Arithmetic operator is consists of 
+// arithmetic operations on numbers
+
+console.log("Addition ",5 + 7)
+console.log("Subtraction ",6 - 3)
+console.log("Multiplication ",7 * 1)
+console.log("Division ",9 / 3)
+console.log("Modulus ",9 % 2)
+console.log("Exponential ",7**2)
+
+
+
+
+// Comparison operator used to compare values
+// and returns boolean values
+console.log(6 == 4)    //equals or not
+console.log(7 > 9)     //greater than
+console.log(7 <= 9)     // less than/equal to,  and so on 
+
+
+
+
+
+// Logical operator used to write logical functions
+// also returns boolean value
+
+8 && 8   // logical and,  returns true if both true else false
+8 || 7   // logical or,  returns true if one of two is true
+!true    // logical not, returns opposite value
+
+
+
+
+
+
+// increment/decrement operator is used on variables to 
+// increase/decrease thier value as postly or previously
+let a = 6
+console.log("default value ", a)
+console.log("post increment/decrement shows changes on same line", a++)
+console.log('after increase in previous line',a)
+console.log("pre decrement/increment shows changes on same line", --a)
+console.log("after decrease in previous line", a)
+
+
+
+
+
+// Bitwise operators are used to do bitwise functions
+// like:  &, |, ~, ^, <<, >>, >>>
