f2ff0ebef4e286a98c01fffca0d8a1873c89bf43
Bikash Nath <splittergame91@gmail.com>
2022-12-01 15:35:27 +0530

    functionality

---
+++ Console.js
@@ -0,0 +1,27 @@
+console.log('shows logs in console')   // log & info are similar
+console.info('shows info in console')
+
+
+console.warn('shows warning in console')  // warning
+
+console.error('shows error in console')     // error
+
+
+
+console.time('counts time')   //starts counting
+// some functions
+console.timeEnd('counts time')  // stops counting & shows time in ms
+
+
+console.table()   // creates table out of array/object
+
+
+
+console.group('group_name')  //create group out of data
+//some elements to add
+//some elements to add
+//some elements to add
+console.groupEnd('group_name')
+
+
+console.clear()  // clears out everything from console
