89140dd6acc97ec8e8574d098ddc38d515153771
Bikash Nath <splittergame91@gmail.com>
2022-12-02 23:33:26 +0530

    Document Object Model

---
+++ DOM.js
@@ -0,0 +1,14 @@
+// DOM manipulation can be shown in web pages
+
+
+console.log(document.body)  // document body of html
+
+
+document.body.style.background = 'red'  // will change the backgrounf color to red
+
+
+ //getElementById is a method & innerHTML is a property
+document.getElementById('demo').innerHTML = "Hello World!";  
+
+
+
