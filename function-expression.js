9e823c12702fb81010f9bb8eef546aa2a20821c1
Bikash Nath <splittergame91@gmail.com>
2022-12-13 23:33:06 +0530

    Function Expression

---
+++ function-expression.js
@@ -0,0 +1,17 @@
+// function expression can be used as IIFE(Imedietely Invoked Function Expression)
+// fuction expression invoked at the time of declaration
+
+
+
+// declaration of function expression
+let reactArea = function (width, height){
+    return "Area of the Reactangle is: " + (width * height);
+}
+
+
+console.log(reactArea(5, 4));
+
+
+
+
+
