dfb6ad5ebbbaf315b6069b00c7c8029ed61b3a9e
Bikash Nath <splittergame91@gmail.com>
2022-12-13 19:05:43 +0530

    importing & console log functions

---
+++ import-export/main.js
@@ -0,0 +1,15 @@
+// importing functions from utility.js
+import {sum, subtract} from "./utility.js";
+
+
+// defining & initializing two variables
+let a=9,b=3;
+
+
+
+
+// invoking imported functions
+console.log('summation function results: ' + sum(a,b));
+console.log('subtraction function results: '+ subtract(a,b))
+
+
