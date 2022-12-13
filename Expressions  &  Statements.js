30b2789764976a1ca5d57ca27e6492f75e1c9dd2
Bikash Nath <splittergame91@gmail.com>
2022-12-13 14:16:54 +0530

    Expressions & Statements in javascript

---
+++ Expressions  &  Statements.js
@@ -0,0 +1,22 @@
+//  a js code that provides a value is Expressions
+
+//examples:
+// let hi = 8
+
+// 1 → produces 1
+// "hello" → produces "hello"
+// 5 * 10 → produces 50
+// num > 100 → produces either true or false
+// isHappy ? "🙂" : "🙁" → produces an emoji
+// [1, 2, 3].pop() → produces the number 3
+
+//-----------------------------------------
+
+
+
+// a js program is a sequence of statements
+// Each statement is an instruction for 
+// computer to do something
+
+//examples: conditionals, functions, loops etc.
+
