8691281e286b5cf4e81eab6c93cdae3a171b9f3c
Bikash Nath <splittergame91@gmail.com>
2023-01-07 21:35:31 +0530

    ecma script 6 (2015)

---
+++ ES6 - Features/ES6.js
@@ -0,0 +1,25 @@
+*    The let keyword
+*    The const keyword
+*    Arrow Functions
+*    The ... Operator  [spread operator]
+*    For/of
+*    Map Objects
+*    Set Objects
+*    Classes
+*    Promises
+*    Symbol
+*    Default Parameters
+*    Function Rest Parameter
+*    String.includes()
+*    String.startsWith()
+*    String.endsWith()
+*    Array.from()
+*    Array keys()
+*    Array find()
+*    Array findIndex()
+*    New Math Methods
+*    New Number Properties
+*    New Number Methods
+*    New Global Methods
+*    Object entries
+*    JavaScript Modules
