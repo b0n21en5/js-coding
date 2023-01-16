300b97c9c6352b707d99968eb4fe5b2cab660909
Bikash Nath <72164894+b0n21en5@users.noreply.github.com>
2023-01-16 22:41:43 +0530

    anonymous function

---
+++ anonymous_function.js
@@ -0,0 +1,15 @@
+// It is a function that does not have 
+// any name associated with it
+
+// An anonymous function is not accessible after its initial 
+// creation, it can only be accessed by a variable
+//  it is stored in as a function as a value
+
+
+
+
+var greet = function (platform) {
+    console.log("This is  ", platform, ' example');
+};
+  
+greet("anonymous function!");
